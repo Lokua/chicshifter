5dbb64b9c55d0e44c7c85e784d30f4e5a34f8eb4
Joshua Kleckner <lokua@lokua.net>
2016-06-25 16:59:46 -0500

    Move Section to v2

--- client/components/Section/Section.jsx
+++ client/components/Section/Section.jsx
@@ -1,25 +1,17 @@
 import React, { Component, PropTypes } from 'react'
 import { connect } from 'react-redux'
-import { injectLogger, selectors } from '@common'
+import find from 'lodash.find'
+
+import { injectLogger } from '@common'
 import { Thumb } from '@components/Thumb'
 import { Prose } from '@components/Prose'
 import css from './Section.scss'
 
 const mapStateToProps = (state, props) => ({
-  section: selectors.section(state, props),
-  sectionV2: (() => {
-    const section = props.params.section
-    let found
-
-    state.issues[0].v2.sections.some(sect => {
-      if (sect.fields.Slug === section) {
-        return (found = sect)
-      }
-    })
-
-    return found
-  })(),
-  content: state.issues[0].v2[props.params.section]
+  sectionV2: find(state.v2.sections,  section => (
+    section.fields.Slug === props.params.section
+  )),
+  content: state.v2[props.params.section]
 })
 
 @injectLogger
@@ -27,14 +19,13 @@ class Section extends Component {
 
   static propTypes = {
     params: PropTypes.object.isRequired,
-    section: PropTypes.object.isRequired,
     sectionV2: PropTypes.object.isRequired,
     content: PropTypes.object.isRequired
   }
 
   render() {
-    const { params, sectionV2, section, content } = this.props
-    const _section = sectionV2.fields
+    const { params, sectionV2, content } = this.props
+    const fields = sectionV2.fields
 
     let data = content.meta ? content.meta : content.data
     if (params.section === 'limiting') {
@@ -45,20 +36,19 @@ class Section extends Component {
 
     return (
       <div className={css.Section}>
-        {_section.hasOwnProperty('Description') && _section.Description &&
+        {fields.hasOwnProperty('Description') && fields.Description &&
           <section className={css.description}>
-            <Prose text={_section.Description} />
+            <Prose text={fields.Description} />
           </section>
         }
         <ul className={css.thumbs}>
           {data && data.map((c, i) => {
             const fields = c.fields
-            const className = section.objectName.split('-')[0]
-            let link = `/issue/${params.issue}/${section.objectName}/`
+            const className = params.section
+            let link = `/issue/${params.issue}/${params.section}/`
             let caption
 
             if (params.section === 'limiting') {
-              this.debug('fields:', fields)
               link += fields.WeekNumber
               caption = `Week ${fields.WeekNumber}: ${fields.Title}`
 
