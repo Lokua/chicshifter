f841cee1e41d9a7971a80ca9269930f41331edaa
Joshua Kleckner <lokua@lokua.net>
2016-09-06 06:01:42 -0500

    Cleanup

--- bin/certbot-renew.sh
+++ bin/certbot-renew.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# /home/web/certbot-auto renew \
-#   --quiet \
-#   --no-self-upgrade \
-#   --post-hook "service nginx restart" > /home/web/certbot-renew.log
-
 /home/web/certbot-auto renew \
   --no-self-upgrade \
   --post-hook "service nginx restart" > /home/web/certbot-renew.log
