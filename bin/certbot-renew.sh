433fada4021d32a4a8c78dd00445c14e559e75e1
Joshua Kleckner <lokua@lokua.net>
2016-09-06 06:00:21 -0500

    Remove quiet option

--- bin/certbot-renew.sh
+++ bin/certbot-renew.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+# /home/web/certbot-auto renew \
+#   --quiet \
+#   --no-self-upgrade \
+#   --post-hook "service nginx restart" > /home/web/certbot-renew.log
+
 /home/web/certbot-auto renew \
-  --quiet \
   --no-self-upgrade \
   --post-hook "service nginx restart" > /home/web/certbot-renew.log
